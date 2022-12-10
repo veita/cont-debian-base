41d5ec5dd00ede2ed0df0dbfe3edaae4d635308c
Alexander Veit <alexander.veit@gmx.net>
2022-12-10 14:22:47 +0100

    Add tmux

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -qy
 apt-get upgrade -qy
-apt-get install -qy locales lsb-release wget curl gnupg2 less vim screen ripgrep tree unzip htop
+apt-get install -qy locales lsb-release wget curl gnupg2 less vim screen tmux ripgrep tree unzip htop
 
 # system configuration: locales
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
@@ -14,19 +14,19 @@ echo 'de_DE.UTF-8 UTF-8' >> /etc/locale.gen
 /usr/sbin/locale-gen
 
 # global shell configuration
-sed -i 's/# "\\e\[5~": history-search-backward/"\\e\[5~": history-search-backward/g' /etc/inputrc
-sed -i 's/# "\\e\[6~": history-search-forward/"\\e\[6~": history-search-forward/g' /etc/inputrc
+sed -i 's|# "\e\[5~": history-search-backward|"\e\[5~": history-search-backward|g' /etc/inputrc
+sed -i 's|# "\e\[6~": history-search-forward|"\e\[6~": history-search-forward|g' /etc/inputrc
 
-sed -i 's/SHELL=\/bin\/sh/SHELL=\/bin\/bash/g' /etc/default/useradd
+sed -i 's|SHELL=/bin/sh|SHELL=/bin/bash|g' /etc/default/useradd
 
-sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /etc/skel/.bashrc
+sed -i 's|#force_color_prompt=yes|force_color_prompt=yes|g' /etc/skel/.bashrc
 
 # global vim configuration
-sed -i 's/"syntax on/syntax on/g' /etc/vim/vimrc
-sed -i 's/"set background=dark/set background=dark/g' /etc/vim/vimrc
+sed -i 's|"syntax on|syntax on|g' /etc/vim/vimrc
+sed -i 's|"set background=dark|set background=dark|g' /etc/vim/vimrc
 
 # global screen configuration
-sed -i 's/#startup_message off/startup_message off/g' /etc/screenrc
+sed -i 's|#startup_message off|startup_message off|g' /etc/screenrc
 echo 'shell /bin/bash' >> /etc/screenrc
 
 # shell settings for root
